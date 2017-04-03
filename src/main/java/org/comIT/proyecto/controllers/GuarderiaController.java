74a735430187044a5fa3fea35b0747f369f69cdf
StevenFCC <valquiria98s@gmail.com>
2017-04-03 11:03:58 -0300

    Modificaciones_b

--- src/main/java/org/comIT/proyecto/controllers/GuarderiaController.java
+++ src/main/java/org/comIT/proyecto/controllers/GuarderiaController.java
@@ -11,7 +11,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import org.comIT.proyecto.connections.VeterinariaDAO;
 import org.comIT.proyecto.entities.User;
 import org.comIT.proyecto.entities.animales.Animal;
 import org.comIT.proyecto.entities.animales.Gallo;
